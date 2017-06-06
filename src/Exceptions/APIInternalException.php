a594fede9285bf98943f4f87fe20b33ffbee03f7
Mike Stevens <mistevs@gmail.com>
2017-06-06 16:31:12 -0500

    making changes to error system

--- src/Exceptions/APIInternalException.php
+++ src/Exceptions/APIInternalException.php
@@ -8,8 +8,8 @@ class APIInternalException extends APIException
 {
 	public function __construct($internalMessage = null, $errorCode = null, $responseCode = 500, $externalMessage = null)
 	{
-		$this->internalMessage = $internalMessage = $internalMessage ?? $this->internalMessage ?? 'An unknown error occurred';
-		$this->externalMessage = $externalMessage = $externalMessage ?? $this->externalMessage ?? 'An unknown error occurred';
+		$this->internalMessage = $internalMessage = (!empty($internalMessage) ? $this->internalMessage : 'An unknown error occurred');
+		$this->externalMessage = $externalMessage = (!empty($externalMessage) ? $this->externalMessage : 'An unknown error occurred');
 
 		return parent::__construct($externalMessage, $errorCode, $responseCode, $internalMessage);
 	}
